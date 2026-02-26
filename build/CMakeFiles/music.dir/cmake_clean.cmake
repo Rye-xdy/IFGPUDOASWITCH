44efa24229afe2a2e2b5d4d6d490216d5aa98f72
RYE_XDY <179372049+Rye-xdy@users.noreply.github.com>
2026-02-26 10:22:32 +0800

    my2

---
+++ build/CMakeFiles/music.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/music.dir/src/uca_2d_interferometer.cu.o"
+  "CMakeFiles/music.dir/src/uca_2d_interferometer.cu.o.d"
+  "music"
+  "music.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/music.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
